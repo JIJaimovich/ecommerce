2b5689b34c3e62fc684e20976f8a254853955913
Juan Ignacio <jaimovich.ignacio@gmail.com>
2022-07-22 22:58:06 -0300

    Desafío Menú e-commerce

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+ 
+const Navbar = () => {
+    return (
+        <nav className='Navbar'>
+           <h1>Ecommerce</h1>
+           <button>Demo1</button>
+           <button>Demo2</button>
+           <button>Demo3</button> 
+        </nav>
+    );
+};
+export default Navbar;
